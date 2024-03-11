a6879780a42b96852f05976791a5be8ed1a52a37
Alexandru Nagy <nagyy.alexandru@gmail.com>
2024-03-11 15:28:59 +0200

    finished example for arrays using new() operator

---
+++ Ex7_MemoryManagement/Example_New_Arrays.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+void NewArrays()
+{
+    int *p = new int[5];
+
+    for(int i = 0; i < 5; ++i)
+    {
+        p[i] = i;
+    }
+
+    delete []p;
+}
+int main(void)
+{
+    NewArrays();
+    return 0;
+}

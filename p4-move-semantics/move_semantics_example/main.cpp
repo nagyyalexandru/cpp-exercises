e5936f3259cd3a487ffd64587a5f39625249c3ed
Alexandru Nagy <nagyy.alexandru@gmail.com>
2024-03-12 14:39:47 +0200

    finished example for Move Constructor, Copy Elision

--- p4-move-semantics/move_semantics_example/main.cpp
+++ p4-move-semantics/move_semantics_example/main.cpp
@@ -5,13 +5,16 @@ Integer Add(const Integer &a, const Integer &b)
 {
     Integer temp;
     temp.SetValue(a.GetValue() + b.GetValue());
-    return temp;
+    return temp;    // this will cal the Move Constructor, for the temporary copy of object
 }
 
 int main()
 {
     Integer a(1), b(3);
     a.SetValue(Add(a, b).GetValue());
-
+    /*  COPY ELISION 
+    on Linux, to be able to see the Move Constructor work, we have to run the command :
+    $ g++ -fno-elide-constructors main.cpp Integer.cpp
+    Then run the 'a.out' file manually      */
     return 0;
 }

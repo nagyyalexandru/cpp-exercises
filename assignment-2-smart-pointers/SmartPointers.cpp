439ddf60437b86a844c5b1c525046dcaff1d0a35
Alexandru Nagy <nagyy.alexandru@gmail.com>
2024-04-03 10:52:06 +0300

    finished demonstration of using shared vs unique pointers

--- assignment-2-smart-pointers/SmartPointers.cpp
+++ assignment-2-smart-pointers/SmartPointers.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 #include <memory>
 
+class Foo
+{
+public:
+    Foo()
+    {
+        std::cout << "Foo created" << std::endl;
+    }
+
+    ~Foo()
+    {
+        std::cout << "Foo destroyed" << std::endl;
+    }
+
+    void msg()
+    {
+        std::cout <<"Hello from Foo() !" << std::endl;
+    }
+};
+
 int main(void)
 {
-    std::cout << "Hello Smart Pointers!" << std::endl;
+    // Example using Shared Pointer
+    std::shared_ptr<Foo> sharedFoo = std::make_shared<Foo>();
+
+    std::cout << "Shared pointer count: " << sharedFoo.use_count() << std::endl;
+
+    std::shared_ptr<Foo> sharedFoo2 = sharedFoo;
+    std::cout << "Shared pointer count: " << sharedFoo.use_count() << std::endl;
+
+    std::cout << "Shared pointer count: " << sharedFoo.use_count() << std::endl;
+
+    // Example using Unique Pointer
+    std::unique_ptr<Foo> uniqueFoo = std::make_unique<Foo>();
+    uniqueFoo->msg();   // Accessing member function
+
+    // This line won't compile, demonstrating exclusive ownership
+    // std::unique_ptr<Foo> uniqueFoo2 = uniqueFoo; 
     return 0;
 }
